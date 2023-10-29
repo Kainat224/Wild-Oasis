53a72ef51983aab78077a17563060d6a5b4c4cfd
kainat <anmolkainat244@gmail.com>
2023-10-30 00:01:01 +0500

    Uploading image

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xnppcnvkjrdeokjlvqit.supabase.co";
+export const supabaseUrl = "https://xnppcnvkjrdeokjlvqit.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhucHBjbnZranJkZW9ramx2cWl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgxNjMwMjUsImV4cCI6MjAxMzczOTAyNX0.CV0EH6C-Y2YXd8ftFF3IYk9Dzh4hSIbTDOaBZgsntGk";
 const supabase = createClient(supabaseUrl, supabaseKey);
