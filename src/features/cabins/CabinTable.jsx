1741fbe3d8450441f959cb30990ba1372d9200b1
kainat224 <anmolkainat244@gmail.com>
2024-08-26 11:26:28 -0700

    API-Side Filtering Bookings

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 const CabinTable = () => {
 
@@ -11,6 +12,7 @@ const CabinTable = () => {
     const [searchParams] = useSearchParams();
 
     if (isLoading) return <Spinner />;
+    if (!cabins.length) return <Empty />
 
     // 1) Filter
     const filterValue = searchParams.get('discount') || 'all';
