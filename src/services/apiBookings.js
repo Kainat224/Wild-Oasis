3cd43cb84d8d7bec4b3fdf5e6c4624e0403b8421
kainat224 <anmolkainat244@gmail.com>
2024-08-27 06:25:48 -0700

    API-Side Sorting Bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,17 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({filter, sortBy}){
- let query = supabase
-  .from("bookings").select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)")
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
+    .from("bookings").select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)")
   // const { data, error } = await supabase.from("bookings").select("*, cabins(*), guests(*)")
 
-  if(filter !== null) query = query[filter.method || "eq"](filter.field, filter.value)
+  if (filter !== null) query = query[filter.method || "eq"](filter.field, filter.value)
+
+  // Sort
+  if (sortBy) query = query.order(sortBy.field, {
+    ascending: sortBy.direction === "asc"
+  })
 
   const { data, error } = await query;
 
