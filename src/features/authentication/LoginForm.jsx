bc74330bf073e729ca4bbb09c84fb3dc8fe8a710
kainat224 <anmolkainat244@gmail.com>
2024-09-02 04:36:46 -0700

    resolve fix in login form

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("kainat@gmail.com");
-  const [password, setPassword] = useState("areeza224");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const {login, isLoading} = useLogin()
 
